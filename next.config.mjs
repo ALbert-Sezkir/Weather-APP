42609852a9663159bc94a025917869e058e96b87
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-04 10:00:20 +0200

    adding and changeing folders

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    reactStrictMode: true,
+    images: {
+        domains: [
+            
+            'images.unsplash.com'
+            
+        ] 
+    }
+
+};
 
 export default nextConfig;
+
