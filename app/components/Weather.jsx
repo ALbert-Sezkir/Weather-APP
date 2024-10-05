2be729e93fe47a77a9c747b16dbc405d958b773d
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-05 11:25:59 +0200

    added style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,9 +6,9 @@ const Weather = ({data}) => {
  console.log('Weather data:', data)
 
   return (
-    <div className='relative flex flex-col justify-between max-w[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
-      <div>
-        <div>
+    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
+      <div className='relative flex justify-between pt-12'>
+        <div className='flex flex-col items-center'>
       <Image 
       src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
       alt='/' 
@@ -17,16 +17,34 @@ const Weather = ({data}) => {
 
       />
 
-      <p>{data.weather[0].main} </p>
+      <p className='text-2xl'>{data.weather[0].main}</p>
         </div>
-        <p>{data.main.temp.toFixed(0)}&#176;</p>
-
-
+        <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
+        </div>
+        {/* Bottom */}
+<div className='relative p-8 rounded-md bg-black/50'>
+  <p className='pb-6 text-2xl text-center '>Weather in {data.name}</p>
+  <div className='flex justify-between text-center'>
+    <div>
+      <p className='text-2xl font-bold'>{data.main.feels_like.toFixed(0)}&#176;</p>
+      <p className='text-xl'>Feels Like</p>
+    </div>
+    <div>
+      <p className='text-2xl font-bold'>{data.main.humidity}%</p>
+      <p className='text-xl'>Humidity</p>
+    </div>
+    <div>
+      <p className='text-2xl font-bold'>{data.wind.speed.toFixed(0)} MPH</p>
+      <p className='text-xl'>Winds</p>
+    </div>
+  </div>
+</div>
 
       </div>
 
 
-    </div>
+
+
   )
 }
 
