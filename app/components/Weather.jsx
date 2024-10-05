a575a02f4d8e0f6ac044c5249f187c22f2e24b67
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-05 11:52:36 +0200

    added style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,9 +6,13 @@ const Weather = ({data}) => {
  console.log('Weather data:', data)
 
   return (
-    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
+    <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[50vh] m-auto p-4 text-gray-300 z-10'>
       <div className='relative flex justify-between pt-12'>
         <div className='flex flex-col items-center'>
+
+
+
+
       <Image 
       src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
       alt='/' 
@@ -22,7 +26,7 @@ const Weather = ({data}) => {
         <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
         </div>
         {/* Bottom */}
-<div className='relative p-8 rounded-md bg-black/50'>
+<div className='relative p-8 rounded-md bg-black/50' >
   <p className='pb-6 text-2xl text-center '>Weather in {data.name}</p>
   <div className='flex justify-between text-center'>
     <div>
