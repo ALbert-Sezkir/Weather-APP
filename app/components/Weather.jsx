7b48f2b1bac7fc931acb8d2d3e73374fef398967
ALbert-Sezkir <a_sezkir@hotmail.com>
2024-10-05 12:24:13 +0200

    added date and style

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,85 +1,56 @@
-
 import Image from 'next/image'
 import React from 'react'
 
-const Weather = ({data}) => {
- console.log('Weather data:', data)
+const Weather = ({ data }) => {
+  console.log('Weather data:', data)
+
+  const { dt, name, main, wind, weather: weatherInfo } = data;
+  const iconUrl = `http://openweathermap.org/img/wn/${weatherInfo[0].icon}@2x.png`;
+  const date = new Date(dt * 1000).toLocaleString('en-GB', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
 
   return (
     <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[50vh] m-auto p-4 text-gray-300 z-10'>
       <div className='relative flex justify-between pt-12'>
         <div className='flex flex-col items-center'>
-
-
-
-
-      <Image 
-      src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
-      alt='/' 
-      width={100}
-      height={100}
-
-      />
-
-      <p className='text-2xl'>{data.weather[0].main}</p>
+          <Image 
+            src={iconUrl} 
+            alt={weatherInfo[0].description} 
+            width={100}
+            height={100}
+          />
+          <p className='text-2xl'>{weatherInfo[0].main}</p>
         </div>
-        <p className='text-9xl'>{data.main.temp.toFixed(0)}&#176;</p>
-        </div>
-        {/* Bottom */}
-<div className='relative p-8 rounded-md bg-black/50' >
-  <p className='pb-6 text-2xl text-center '>Weather in {data.name}</p>
-  <div className='flex justify-between text-center'>
-    <div>
-      <p className='text-2xl font-bold'>{data.main.feels_like.toFixed(0)}&#176;</p>
-      <p className='text-xl'>Feels Like</p>
-    </div>
-    <div>
-      <p className='text-2xl font-bold'>{data.main.humidity}%</p>
-      <p className='text-xl'>Humidity</p>
-    </div>
-    <div>
-      <p className='text-2xl font-bold'>{data.wind.speed.toFixed(0)} MPH</p>
-      <p className='text-xl'>Winds</p>
-    </div>
-  </div>
-</div>
-
+        <p className='text-9xl'>{main.temp.toFixed(0)}&#176;</p>
       </div>
-
-
-
-
+      {/* Bottom */}
+      <div className='relative p-8 rounded-md bg-black/50'>
+        <p className='pb-6 text-2xl text-center'>Weather in {name}</p>
+        <p className='pb-6 text-xl text-center'>{date}</p>
+        <div className='flex justify-between text-center'>
+          <div>
+            <p className='text-2xl font-bold'>{main.feels_like.toFixed(0)}&#176;</p>
+            <p className='text-xl'>Feels Like</p>
+            <p>{weatherInfo[0].description.charAt(0).toUpperCase() + weatherInfo[0].description.slice(1)}.</p>
+          </div>
+          <div>
+            <p className='text-2xl font-bold'>{main.humidity}%</p>
+            <p className='text-xl'>Humidity</p>
+          </div>
+          <div>
+            <p className='text-2xl font-bold'>{wind.speed.toFixed(0)} MPH</p>
+            <p className='text-xl'>Winds</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
 export default Weather
-
-// import Image from 'next/image';
-// import React from 'react';
-
-// const Weather = ({ data }) => {
-//   console.log(data);
-
-//   // Check if data and data.weather are defined
-//   if (!data || !data.weather || !data.weather[0]) {
-//     return <p>No weather data available</p>;
-//   }
-
-//   return (
-//     <div>
-//       <div>
-//         <div>
-//           <Image 
-//             src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} 
-//             alt={data.weather[0].description} 
-//             width={100} 
-//             height={100} 
-//           />
-//           <p>{data.weather[0].main}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Weather;
